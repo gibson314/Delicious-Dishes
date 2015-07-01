dde68a082b189a621a1ba6a7bb083773cfe00bd5
liuyuang <liuyuang1994@gmail.com>
2015-07-01 18:55:10 +0800

    authorname

--- resources/views/dishes/index.blade.php
+++ resources/views/dishes/index.blade.php
@@ -4,7 +4,8 @@
         @foreach ($dishes as $dish)
             <h2><a href="{{ url('/dishes',$dish->id) }}"><img src="{{$dish->TitleImg}}"/></a></h2>
             <h2><a href="{{ url('/dishes',$dish->id) }}">{{$dish->name}}</a></h2>
-            <p>{{$dish->author}}发表于{{$dish->publish_date}}<br></p>
+            <?php $author=\App\User::where('id',$dish->authorid)->first();?>
+            <p><a href="{{ url('author',$dish->authorid) }}">{{$author->username}}</a> 发表于{{$dish->publish_date}}<br></p>
             <p>{{$dish->intro}}<br></p>
         @endforeach
         {{$dishes->render()}}
